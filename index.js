05a0ca27e7bfa1200d8b80204b2bf65cbce63b6c
Kim Love <kimberlymlove15@gmail.com>
2023-08-02 13:36:42 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: "Kim",
+  streetAddress: "240 M St SW",
+};
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+  const newObj = { ...employeeObj };
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+  employeeObj[key] = value;
+  return employeeObj;
+}
+
+function deleteFromEmployeeByKey(employeeObj, key){
+    const newObj = {...employeeObj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key){
+    delete employeeObj[key];
+    return employeeObj;
+}
